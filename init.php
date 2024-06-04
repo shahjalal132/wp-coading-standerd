d9c4e9f3b4310f37885cb5f7ac654cf2cfee05f9
shahjalal132 <rjshahjalal132@gmail.com>
2024-06-04 16:52:21 +0600

    formate codes

--- init.php
+++ init.php
@@ -9,6 +9,7 @@
  * License: GPL2 or later.
  * text-domain: query-apis
  * @package wpcs
+ * 
  */
 
 // If this file is access directly, abort!!!
@@ -16,61 +17,36 @@ defined( 'ABSPATH' ) or die( 'Unauthorized Access' );
 
 function techiepress_get_send_data() {
 
-    $url = 'https://jsonplaceholder.typicode.com/users';$arguments = 
-	array('method' => 'GET');
-	$response = wp_remote_get( 
-		
-		
-		$url, $arguments 
-	
-	);
+    $url       = 'https://jsonplaceholder.typicode.com/users';
+    $arguments = array( 'method' => 'GET' );
+    $response  = wp_remote_get( $url, $arguments );
 
-	if (is_wp_error($response)){
-		$error_message = 
-		
-
-		
-		$response->get_error_message();return
-		
-		
-		"Something went wrong: $error_message"; } else {
-		echo '<pre>'; var_dump( 
-			
-			wp_remote_retrieve_body( 
-			
-			
-			$response ) ); echo '</pre>';
-	}}	
+    if ( is_wp_error( $response ) ) {
+        $error_message = $response->get_error_message();
+        return
+            "Something went wrong: $error_message";
+    } else {
+        echo '<pre>';
+        var_dump(
+            wp_remote_retrieve_body( $response )
+        );
+        echo '</pre>';
+    }
+}
 
 /**
  * Register a custom menu page to view the information queried.
  */
-function 
-
-techiepress_register_my_custom_menu_page() {
-	add_menu_page(
-		__( 
-			
-
-
-			
-			'Query API Test Settings', 'query-apis' ),
-		'Query API Test',
-		'manage_options',
-
-
-		'api-test.php','techiepress_get_send_data','dashicons-testimonial',
-		16
-	);
+function techiepress_register_my_custom_menu_page() {
+    add_menu_page(
+        __( 'Query API Test Settings', 'query-apis' ),
+        'Query API Test',
+        'manage_options',
+        'api-test.php',
+        'techiepress_get_send_data',
+        'dashicons-testimonial',
+        16
+    );
 }
 
-add_action( 
-	'admin_menu', 'techiepress_register_my_custom_menu_page' 
-
-
-
-);
-
-/**
- * added whitespace
- */
+add_action( 'admin_menu', 'techiepress_register_my_custom_menu_page' );
